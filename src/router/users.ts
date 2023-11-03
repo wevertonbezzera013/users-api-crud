5aeada15ec1739a9dd28df45d65ded35163869b2
Weverton <wevertonbezzera013@gmail.com>
2023-11-03 02:55:10 -0300

    feat: login authentication

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,10 +1,10 @@
 import express from "express";
 
 import { deleteUser, getAllUsers, updateUser } from "../controllers/users";
-import { canDeleteUser } from "../middlewares";
+import { canDeleteUser, isAuthenticated } from "../middlewares";
 
 export default (router: express.Router) => {
-  router.get("/users", getAllUsers);
-  router.delete("/users/:id", canDeleteUser, deleteUser);
-  router.patch("/users/:id", updateUser);
+  router.get("/users", isAuthenticated, getAllUsers);
+  router.delete("/users/:id", isAuthenticated, canDeleteUser, deleteUser);
+  router.patch("/users/:id", isAuthenticated, updateUser);
 };
