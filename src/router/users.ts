03f30fb2b8012db56f1711ca9e0a6850f7547211
Weverton <wevertonbezzera013@gmail.com>
2023-11-03 02:24:40 -0300

    fix: cors issues

--- src/router/users.ts
+++ src/router/users.ts
@@ -4,7 +4,7 @@ import { deleteUser, getAllUsers, updateUser } from "../controllers/users";
 import { canDeleteUser, isAuthenticated } from "../middlewares";
 
 export default (router: express.Router) => {
-  router.get("/users", isAuthenticated, getAllUsers);
+  router.get("/users", getAllUsers);
   router.delete("/users/:id", isAuthenticated, canDeleteUser, deleteUser);
   router.patch("/users/:id", isAuthenticated, updateUser);
 };
