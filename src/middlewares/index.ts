15b303944feccd1be5e5c0134870db24d6ce3bbf
Weverton <wevertonbezzera013@gmail.com>
2023-11-03 02:29:51 -0300

    fix: remove unessessary code

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,32 +1,5 @@
 import express from "express";
-import { merge } from "lodash";
-import { getUserById, getUserBySessionToken } from "../db/users";
-
-export const isAuthenticated = async (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) => {
-  try {
-    const sessionToken = req.cookies["APP-AUTH"];
-
-    if (!sessionToken) {
-      return res.sendStatus(400);
-    }
-
-    const existingUser = await getUserBySessionToken(sessionToken);
-
-    if (!existingUser) {
-      return res.sendStatus(403);
-    }
-
-    merge(req, { identity: existingUser });
-    return next();
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
-  }
-};
+import { getUserById } from "../db/users";
 
 interface AuthenticatedRequest extends express.Request {
   identity?: Record<string, any>;
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,10 +1,10 @@
 import express from "express";
 
 import { deleteUser, getAllUsers, updateUser } from "../controllers/users";
-import { canDeleteUser, isAuthenticated } from "../middlewares";
+import { canDeleteUser } from "../middlewares";
 
 export default (router: express.Router) => {
-  router.get("/users", isAuthenticated, getAllUsers);
-  router.delete("/users/:id", isAuthenticated, canDeleteUser, deleteUser);
-  router.patch("/users/:id", isAuthenticated, updateUser);
+  router.get("/users", getAllUsers);
+  router.delete("/users/:id", canDeleteUser, deleteUser);
+  router.patch("/users/:id", updateUser);
 };
