11213f2ca6aaba84b26a642652a8f60b12612381
Weverton <wevertonbezzera013@gmail.com>
2023-11-03 01:58:53 -0300

    fix: cors issues

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "https://users-api-crud.vercel.app",
+    origin: "https://users-api-crud.vercel.app/users",
     credentials: true,
   })
 );
