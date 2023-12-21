990f5e39ee19ad7f8e3566d288d2985cb367b170
Weverton <wevertonbezzera013@gmail.com>
2023-12-21 00:31:23 -0300

    Update Swagger UI CSS URL to JS URL

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import swaggerDocs from './swagger.json'
 dotenv.config()
 
 const CSS_URL =
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.css'
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.js'
 
 const app = express()
 
